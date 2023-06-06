144c02ed53731466ac1f49b4c38cfc4ae42f3338
manSlayer16 <120093141+manSlayer16@users.noreply.github.com>
2023-06-06 14:57:17 +0100

    Update pop.py

--- pop.py
+++ pop.py
@@ -1,8 +1,19 @@
 Population = 2
 PopGrowth = 1.005
 Days = 0
+Farmers = 0
+Food = 0
+
+
 while Population <= 1000000:
   Days += 1
   If Days == 56:
-    Population *= PopGrowth
+    Food = Farmers * 10
+    If Population < Food:
+      Food = Food - Population
+      Population *= PopGrowth
+    Else:
+      Population = Food
+      Food = 0
     Days = 0
+    
